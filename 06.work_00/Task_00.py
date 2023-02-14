2c935621fe959caef68057802d54dcf6ff70612e
Lupin <skorpion_94@list.ru>
2023-02-14 17:16:06 +0100

    Немного о функции sort и sorted

---
+++ 06.work_00/Task_00.py
@@ -0,0 +1,35 @@
+# a = [2,46,8,9,54]
+# a.sort()
+# print(a)
+#____________________________
+# a = "sdf fdj shrtj er nv"
+# r = sorted(a)
+# print(r)
+# #____________________________
+# a = (4,7,9,2)
+# r = sorted(a)
+# print(r)
+# #____________________________
+# a = [12,34,567,671]
+# r = sorted(a,reverse=True)
+# print(r)
+# # #____________________________
+# a = [12,34,567,671]
+# def sorting(x):
+#     if x%2 == 0:
+#         return True
+#     else:
+#         return x
+    
+# r = sorted(a,key = sorting )
+# print(r)
+# #____________________________
+# a = ["sd", "wefe","gsd","d"]
+
+# r = sorted(a,key = lambda x:x[0])
+# print(r)
+# #____________________________
+a = {4:'as',2:'bd',3:'ve'}
+
+r = sorted(a.keys())
+print(r)
